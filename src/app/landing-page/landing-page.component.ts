c781d0ad5d699d22bce0b08e164c4238c43930dd
Shankar <shankarshanmugam00@gmail.com>
2025-06-09 14:52:05 +0530

    commit after landingpage correction

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -3,7 +3,6 @@ import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [RouterLink],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
